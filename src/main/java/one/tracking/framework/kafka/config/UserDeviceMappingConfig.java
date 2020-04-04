23f207d15754f0f87a7fed8d3ce598d12b24600d
Florian <7901907+mrRo8o7@users.noreply.github.com>
2020-04-04 11:30:11 +0200

    Deleted Push Notification Consumer Beans

--- src/main/java/one/tracking/framework/kafka/config/UserDeviceMappingConfig.java
+++ src/main/java/one/tracking/framework/kafka/config/UserDeviceMappingConfig.java
@@ -1,13 +1,12 @@
 package one.tracking.framework.kafka.config;
 
 import one.tracking.framework.kafka.events.UserDeviceMapping;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.*;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 
 @Configuration
@@ -21,22 +20,5 @@ public class UserDeviceMappingConfig extends KafkaConfigs {
     @Bean(name = "kafka.template.UserDeviceMapping")
     public KafkaTemplate<String, UserDeviceMapping> userDeviceMappingKafkaTemplate() {
         return new KafkaTemplate<>(userDeviceMappingProducerFactory());
-    }
-
-    @Bean(name = "kafka.consumer.UserDeviceMapping")
-    public ConsumerFactory<String, UserDeviceMapping> userDeviceMappingFactory() {
-        return new DefaultKafkaConsumerFactory<>(
-                consumerConfig(),
-                new StringDeserializer(),
-                new JsonDeserializer<>(UserDeviceMapping.class));
-    }
-
-    @Bean(name = "kafka.listener.UserDeviceMapping")
-    public ConcurrentKafkaListenerContainerFactory<String, UserDeviceMapping> listenerContainerFactory() {
-
-        final ConcurrentKafkaListenerContainerFactory<String, UserDeviceMapping> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(userDeviceMappingFactory());
-        return factory;
     }
 }
--- src/main/java/one/tracking/framework/kafka/config/UserDeviceMappingDeletionConfig.java
+++ src/main/java/one/tracking/framework/kafka/config/UserDeviceMappingDeletionConfig.java
@@ -1,13 +1,12 @@
 package one.tracking.framework.kafka.config;
 
 import one.tracking.framework.kafka.events.UserDeviceMappingDeletion;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.*;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 
 @Configuration
@@ -21,22 +20,5 @@ public class UserDeviceMappingDeletionConfig extends KafkaConfigs {
     @Bean(name = "kafka.template.UserDeviceMappingDeletion")
     public KafkaTemplate<String, UserDeviceMappingDeletion> userDeviceMappingDeletionKafkaTemplate() {
         return new KafkaTemplate<>(userDeviceMappingDeletionProducerFactory());
-    }
-
-    @Bean(name = "kafka.consumer.UserDeviceMappingDeletion")
-    public ConsumerFactory<String, UserDeviceMappingDeletion> userDeviceMappingDeletionFactory() {
-        return new DefaultKafkaConsumerFactory<>(
-                consumerConfig(),
-                new StringDeserializer(),
-                new JsonDeserializer<>(UserDeviceMappingDeletion.class));
-    }
-
-    @Bean(name = "kafka.listener.UserDeviceMappingDeletion")
-    public ConcurrentKafkaListenerContainerFactory<String, UserDeviceMappingDeletion> listenerContainerFactory() {
-
-        final ConcurrentKafkaListenerContainerFactory<String, UserDeviceMappingDeletion> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(userDeviceMappingDeletionFactory());
-        return factory;
     }
 }
